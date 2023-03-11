8c8900b1f123126cddbb4c0521cca3310fbcd06f
Gennady Trafimenkov <gennady.trafimenkov@gmail.com>
2023-03-11 07:20:08 +0000

    Keep BLANK_TOWN out of the proper code

--- rustlib/src/exp_towns.rs
+++ rustlib/src/exp_towns.rs
@@ -1,3 +1,5 @@
+use crate::towns;
+
 #[repr(C)]
 #[allow(non_camel_case_types)]
 pub enum TownID {
@@ -16,19 +18,53 @@ pub enum TownID {
     CHITZENA = 12,
 }
 
+impl TownID {
+    #[allow(dead_code)]
+    fn from_internal(value: towns::Town) -> Self {
+        match value {
+            towns::Town::Omerta => TownID::OMERTA,
+            towns::Town::Drassen => TownID::DRASSEN,
+            towns::Town::Alma => TownID::ALMA,
+            towns::Town::Grumm => TownID::GRUMM,
+            towns::Town::Tixa => TownID::TIXA,
+            towns::Town::Cambria => TownID::CAMBRIA,
+            towns::Town::SanMona => TownID::SAN_MONA,
+            towns::Town::Estoni => TownID::ESTONI,
+            towns::Town::Orta => TownID::ORTA,
+            towns::Town::Balime => TownID::BALIME,
+            towns::Town::Meduna => TownID::MEDUNA,
+            towns::Town::Chitzena => TownID::CHITZENA,
+        }
+    }
+    const fn to_internal(&self) -> towns::Town {
+        match self {
+            TownID::OMERTA => towns::Town::Omerta,
+            TownID::DRASSEN => towns::Town::Drassen,
+            TownID::ALMA => towns::Town::Alma,
+            TownID::GRUMM => towns::Town::Grumm,
+            TownID::TIXA => towns::Town::Tixa,
+            TownID::CAMBRIA => towns::Town::Cambria,
+            TownID::SAN_MONA => towns::Town::SanMona,
+            TownID::ESTONI => towns::Town::Estoni,
+            TownID::ORTA => towns::Town::Orta,
+            TownID::BALIME => towns::Town::Balime,
+            TownID::MEDUNA => towns::Town::Meduna,
+            TownID::CHITZENA => towns::Town::Chitzena,
+            TownID::BLANK_SECTOR => {
+                // this inconsistency should not leak to the proper code
+                panic!("an attempt to convert BLANK_SECTOR to a proper town");
+            }
+        }
+    }
+}
+
 /// Is militia training allowed in this town?
 #[no_mangle]
 pub extern "C" fn MilitiaTrainingAllowedInTown(town: TownID) -> bool {
-    matches!(
-        town,
-        TownID::DRASSEN
-            | TownID::ALMA
-            | TownID::GRUMM
-            | TownID::CAMBRIA
-            | TownID::BALIME
-            | TownID::MEDUNA
-            | TownID::CHITZENA
-    )
+    match town {
+        TownID::BLANK_SECTOR => false,
+        _ => town.to_internal().is_militia_training_allowed(),
+    }
 }
 
 #[cfg(test)]
--- rustlib/src/towns.rs
+++ rustlib/src/towns.rs
@@ -1,15 +1,42 @@
-// pub enum Town {
-//     BLANK_SECTOR = 0,
-//     OMERTA = 1,
-//     DRASSEN = 2,
-//     ALMA = 3,
-//     GRUMM = 4,
-//     TIXA = 5,
-//     CAMBRIA = 6,
-//     SAN_MONA = 7,
-//     ESTONI = 8,
-//     ORTA = 9,
-//     BALIME = 10,
-//     MEDUNA = 11,
-//     CHITZENA = 12,
-// }
+/// Town IDs.
+/// It is almost the same as exp_towns::TownID, but without BLANK_SECTOR.
+pub enum Town {
+    Omerta = 1,
+    Drassen = 2,
+    Alma = 3,
+    Grumm = 4,
+    Tixa = 5,
+    Cambria = 6,
+    SanMona = 7,
+    Estoni = 8,
+    Orta = 9,
+    Balime = 10,
+    Meduna = 11,
+    Chitzena = 12,
+}
+
+impl Town {
+    pub const fn is_militia_training_allowed(&self) -> bool {
+        matches!(
+            self,
+            Town::Drassen
+                | Town::Alma
+                | Town::Grumm
+                | Town::Cambria
+                | Town::Balime
+                | Town::Meduna
+                | Town::Chitzena
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn militia_training_allowed() {
+        assert_eq!(false, Town::Tixa.is_militia_training_allowed());
+        assert_eq!(true, Town::Meduna.is_militia_training_allowed());
+    }
+}
