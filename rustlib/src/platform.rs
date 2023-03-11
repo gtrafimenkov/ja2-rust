e274e44a1ce96514ae25c2e6bc3fdccbd82e0f37
Gennady Trafimenkov <gennady.trafimenkov@gmail.com>
2023-03-11 12:42:59 +0300

    Fix the code to remove read-only permission on Linux

--- rustlib/src/platform.rs
+++ rustlib/src/platform.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::{os::unix::prelude::PermissionsExt, path::PathBuf};
 
 /// Remove all files in a directory, but not the directory itself or any subdirectories.
 pub fn remove_files_in_directory(path: &PathBuf) -> std::io::Result<()> {
@@ -13,9 +13,18 @@ pub fn remove_files_in_directory(path: &PathBuf) -> std::io::Result<()> {
 }
 
 // Remove read-only attribute from a file.
+#[cfg(target_os = "windows")]
 pub fn remove_readonly_attribute(path: &PathBuf) -> std::io::Result<()> {
     let mut perms = std::fs::metadata(path)?.permissions();
     perms.set_readonly(false);
     std::fs::set_permissions(path, perms)?;
+    Ok(())
+}
+
+// Remove read-only attribute from a file.
+#[cfg(not(target_os = "windows"))]
+pub fn remove_readonly_attribute(path: &PathBuf) -> std::io::Result<()> {
+    let mut perms = std::fs::metadata(path)?.permissions();
+    perms.set_mode(0o600);
     Ok(())
 }
