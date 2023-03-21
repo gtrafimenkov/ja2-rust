c80a62d0e4f78bdcefa9bdba2ad015ac13d9edf5
Gennady Trafimenkov <gennady.trafimenkov@gmail.com>
2023-03-21 05:50:50 +0000

    State 2023-03-21 05:50:50

--- ja2lib/SGP/VSurfaceInternal.h
+++ ja2lib/SGP/VSurfaceInternal.h
@@ -7,20 +7,31 @@
 #include "SGP/VSurface.h"
 
 struct VSurface {
-  UINT16 usHeight;         // Height of Video Surface
-  UINT16 usWidth;          // Width of Video Surface
-  UINT8 ubBitDepth;        // BPP ALWAYS 16!
-  PTR pSurfaceData;        // A void pointer, but for this implementation, is really a
-                           // lpDirectDrawSurface;
-  PTR pSurfaceData1;       // Direct Draw One Interface
-  PTR pSavedSurfaceData1;  // A void pointer, but for this implementation, is really a
-                           // lpDirectDrawSurface; pSavedSurfaceData is used to hold all video
-                           // memory Surfaces so that they my be restored
-  PTR pSavedSurfaceData;   // A void pointer, but for this implementation, is really a
-                          // lpDirectDrawSurface; pSavedSurfaceData is used to hold all video memory
-                          // Surfaces so that they my be restored
+  UINT16 usHeight;   // Height of Video Surface
+  UINT16 usWidth;    // Width of Video Surface
+  UINT8 ubBitDepth;  // BPP ALWAYS 16!
+
+  // A void pointer, but for this implementation, is really a
+  // lpDirectDrawSurface;
+  PTR pSurfaceData;
+
+  // Direct Draw One Interface
+  PTR pSurfaceData1;
+
+  // A void pointer, but for this implementation, is really a
+  // lpDirectDrawSurface; pSavedSurfaceData is used to hold all video
+  // memory Surfaces so that they my be restored
+  PTR pSavedSurfaceData1;
+
+  // A void pointer, but for this implementation, is really a
+  // lpDirectDrawSurface; pSavedSurfaceData is used to hold all video memory
+  // Surfaces so that they my be restored
+  PTR pSavedSurfaceData;
+
+  // A void pointer, but for this implementation a DDPalette
+  PTR pPalette;
+
   UINT32 fFlags;              // Used to describe memory usage, etc
-  PTR pPalette;               // A void pointer, but for this implementation a DDPalette
   UINT16 *p16BPPPalette;      // A 16BPP palette used for 8->16 blits
   COLORVAL TransparentColor;  // Defaults to 0,0,0
   PTR pClipper;               // A void pointer encapsolated as a clipper Surface
