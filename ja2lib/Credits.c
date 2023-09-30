gtrafimenkov/ja2-rust

commit 67da271bf3dd5cde9da14d6c2d7159cdca66b5f3

Remove transparent color from vsurface (#35)
* State 2023-07-05 18:46:24

* State 2023-07-05 20:16:00

ja2lib/Credits.c
@@ -700,9 +700,6 @@ BOOLEAN AddCreditNode(UINT32 uiType, UINT32 uiFlags, STR16 pString) {
       return (FALSE);
     }
 
-    // Set transparency
-    SetVideoSurfaceTransparency(pNodeToAdd->uiVideoSurfaceImage, 0);
-
     // fill the surface with a transparent color
     VSurfaceColorFill(GetVSByID(pNodeToAdd->uiVideoSurfaceImage), 0, 0, CRDT_WIDTH_OF_TEXT_AREA,
                       pNodeToAdd->sHeightOfString, 0);

ja2lib/SGP/VSurface.c
@@ -404,7 +404,6 @@ static uint32_t addVSurfaceToList(struct VSurface *vs) {
 struct VSurface *VSurfaceAdd(u16 width, u16 height, VSurfID *puiIndex) {
   struct VSurface *vs = CreateVideoSurface(width, height);
   if (vs) {
-    SetVideoSurfaceTransparencyColor(vs, FROMRGB(0, 0, 0));
     if (puiIndex) {
       *puiIndex = addVSurfaceToList(vs);
     }
@@ -423,7 +422,6 @@ BOOLEAN AddVideoSurface(VSURFACE_DESC *desc, VSurfID *puiIndex) {
     return FALSE;
   }
 
-  SetVideoSurfaceTransparencyColor(vs, FROMRGB(0, 0, 0));
   *puiIndex = addVSurfaceToList(vs);
   return TRUE;
 }
@@ -433,26 +431,6 @@ BYTE *VSurfaceLockOld(struct VSurface *vs, u32 *pitch) {
   struct BufferLockInfo res = VSurfaceLock(vs);
   *pitch = res.pitch;
   return res.dest;
-}
-
-BOOLEAN SetVideoSurfaceTransparency(UINT32 uiIndex, COLORVAL TransColor) {
-  struct VSurface *hVSurface;
-
-  //
-  // Get Video Surface
-  //
-
-  if (!(GetVideoSurface(&hVSurface, uiIndex))) {
-    return FALSE;
-  }
-
-  //
-  // Set transparency
-  //
-
-  SetVideoSurfaceTransparencyColor(hVSurface, TransColor);
-
-  return (TRUE);
 }
 
 struct VSurface *GetVSByID(VSurfID id) {

ja2lib/SGP/VSurface.h
@@ -77,9 +77,6 @@ struct VSurface *GetVSByID(VSurfID id);
 
 BOOLEAN VSurfaceColorFill(struct VSurface *dest, i32 x1, i32 y1, i32 x2, i32 y2, u16 Color16BPP);
 
-// Sets transparency
-BOOLEAN SetVideoSurfaceTransparency(UINT32 uiIndex, COLORVAL TransColor);
-
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 //
 // Video Surface manipulation functions
@@ -89,9 +86,6 @@ BOOLEAN SetVideoSurfaceTransparency(UINT32 uiIndex, COLORVAL TransColor);
 // Set data from struct Image*.
 BOOLEAN SetVideoSurfaceDataFromHImage(struct VSurface *hVSurface, struct Image *hImage, UINT16 usX,
                                       UINT16 usY, struct GRect *pSrcRect);
-
-// Sets Transparency color into HVSurface and the underlying DD surface
-BOOLEAN SetVideoSurfaceTransparencyColor(struct VSurface *hVSurface, COLORVAL TransColor);
 
 // Deletes all data, including palettes, regions, DD Surfaces
 BOOLEAN DeleteVideoSurface(struct VSurface *hVSurface);

ja2lib/SGP/VSurfaceInternal.h
@@ -13,8 +13,6 @@ struct VSurface {
 
   // Direct Draw One Interface
   PTR pSurfaceData1;
-
-  COLORVAL TransparentColor;  // Defaults to 0,0,0
 };
 
 #endif

ja2lib/Tactical/Interface.c
@@ -1479,53 +1479,6 @@ void RenderOverlayMessage(VIDEO_OVERLAY *pBlitter) {
                    pBlitter->sY + gusOverlayPopupBoxHeight);
 }
 
-void BeginOverlayMessage(UINT32 uiFont, CHAR16 *pFontString, ...) {
-  va_list argptr;
-  VIDEO_OVERLAY_DESC VideoOverlayDesc;
-  wchar_t SlideString[512];
-
-  va_start(argptr, pFontString);  // Set up variable argument pointer
-  vswprintf(SlideString, ARR_SIZE(SlideString), pFontString,
-            argptr);  // process gprintf string (get output str)
-  va_end(argptr);
-
-  // Override it!
-  OverrideMercPopupBox(&gpOverrideMercBox);
-
-  SetPrepareMercPopupFlags(MERC_POPUP_PREPARE_FLAGS_TRANS_BACK | MERC_POPUP_PREPARE_FLAGS_MARGINS);
-
-  // Prepare text box
-  iOverlayMessageBox = PrepareMercPopupBox(iOverlayMessageBox, BASIC_MERC_POPUP_BACKGROUND,
-                                           RED_MERC_POPUP_BORDER, SlideString, 200, 50, 0, 0,
-                                           &gusOverlayPopupBoxWidth, &gusOverlayPopupBoxHeight);
-
-  // Set it back!
-  ResetOverrideMercPopupBox();
-
-  if (giPopupSlideMessageOverlay == -1) {
-    // Set Overlay
-    VideoOverlayDesc.sLeft = (640 - gusOverlayPopupBoxWidth) / 2;
-    VideoOverlayDesc.sTop = 100;
-    VideoOverlayDesc.sRight = VideoOverlayDesc.sLeft + gusOverlayPopupBoxWidth;
-    VideoOverlayDesc.sBottom = VideoOverlayDesc.sTop + gusOverlayPopupBoxHeight;
-    VideoOverlayDesc.sX = VideoOverlayDesc.sLeft;
-    VideoOverlayDesc.sY = VideoOverlayDesc.sTop;
-    VideoOverlayDesc.BltCallback = RenderOverlayMessage;
-
-    giPopupSlideMessageOverlay = RegisterVideoOverlay(0, &VideoOverlayDesc);
-  }
-}
-
-void EndOverlayMessage() {
-  if (giPopupSlideMessageOverlay != -1) {
-    //		DebugMsg( TOPIC_JA2, DBG_ERROR, String( "Removing Overlay message") );
-
-    RemoveVideoOverlay(giPopupSlideMessageOverlay);
-
-    giPopupSlideMessageOverlay = -1;
-  }
-}
-
 void DrawBarsInUIBox(struct SOLDIERTYPE *pSoldier, INT16 sXPos, INT16 sYPos, INT16 sWidth,
                      INT16 sHeight) {
   FLOAT dWidth, dPercentage;
@@ -2202,9 +2155,6 @@ void InternalBeginUIMessage(BOOLEAN fUseSkullIcon, CHAR16 *pFontString, ...) {
 
   // Override it!
   OverrideMercPopupBox(&gpUIMessageOverrideMercBox);
-
-  // SetPrepareMercPopupFlags( MERC_POPUP_PREPARE_FLAGS_TRANS_BACK |
-  // MERC_POPUP_PREPARE_FLAGS_MARGINS );
 
   if (fUseSkullIcon) {
     SetPrepareMercPopupFlags(MERC_POPUP_PREPARE_FLAGS_MARGINS | MERC_POPUP_PREPARE_FLAGS_SKULLICON);

ja2lib/Tactical/Interface.h
@@ -126,9 +126,6 @@ void CancelOpenDoorMenu();
 
 void HandleInterfaceBackgrounds();
 
-void BeginOverlayMessage(UINT32 uiFont, CHAR16 *pFontString, ...);
-void EndOverlayMessage();
-
 void DrawSelectedUIAboveGuy(UINT16 usSoldierID);
 
 void CreateCurrentTacticalPanelButtons();

ja2lib/Utils/MercTextBox.c
@@ -369,10 +369,6 @@ INT32 PrepareMercPopupBox(INT32 iBoxId, UINT8 ubBackgroundIndex, UINT8 ubBorderI
   DestRect.iBottom = DestRect.iTop + usHeight;
 
   if (pPopUpTextBox->uiFlags & MERC_POPUP_PREPARE_FLAGS_TRANS_BACK) {
-    // Zero with yellow,
-    // Set source transparcenty
-    SetVideoSurfaceTransparency(pPopUpTextBox->uiSourceBufferIndex, FROMRGB(255, 255, 0));
-
     pDestBuf =
         (UINT16 *)VSurfaceLockOld(GetVSByID(pPopUpTextBox->uiSourceBufferIndex), &uiDestPitchBYTES);
 

platform-dummy/dummy-platform.c
@@ -150,10 +150,6 @@ BOOLEAN Set8BPPPalette(struct SGPPaletteEntry *pPalette) { return FALSE; }
 
 void FatalError(STR8 pError, ...) {}
 
-BOOLEAN SetVideoSurfaceTransparencyColor(struct VSurface *hVSurface, COLORVAL TransColor) {
-  return FALSE;
-}
-
 struct VSurface *CreateVideoSurface(u16 width, u16 height) { return NULL; }
 
 BOOLEAN DeleteVideoSurface(struct VSurface *hVSurface) { return FALSE; }

commit bbd6bb3f26ce27ec3b42446905eeacb68499d646

Restore broken video call distortion
It was broken in 67da271bf3dd5cde9da14d6c2d7159cdca66b5f3

ja2lib/Credits.c
@@ -700,6 +700,9 @@ BOOLEAN AddCreditNode(UINT32 uiType, UINT32 uiFlags, STR16 pString) {
       return (FALSE);
     }
 
+    // Set transparency
+    SetVideoSurfaceTransparency(pNodeToAdd->uiVideoSurfaceImage, 0);
+
     // fill the surface with a transparent color
     VSurfaceColorFill(GetVSByID(pNodeToAdd->uiVideoSurfaceImage), 0, 0, CRDT_WIDTH_OF_TEXT_AREA,
                       pNodeToAdd->sHeightOfString, 0);

ja2lib/SGP/VSurface.c
@@ -404,6 +404,7 @@ static uint32_t addVSurfaceToList(struct VSurface *vs) {
 struct VSurface *VSurfaceAdd(u16 width, u16 height, VSurfID *puiIndex) {
   struct VSurface *vs = CreateVideoSurface(width, height);
   if (vs) {
+    SetVideoSurfaceTransparencyColor(vs, FROMRGB(0, 0, 0));
     if (puiIndex) {
       *puiIndex = addVSurfaceToList(vs);
     }
@@ -422,6 +423,7 @@ BOOLEAN AddVideoSurface(VSURFACE_DESC *desc, VSurfID *puiIndex) {
     return FALSE;
   }
 
+  SetVideoSurfaceTransparencyColor(vs, FROMRGB(0, 0, 0));
   *puiIndex = addVSurfaceToList(vs);
   return TRUE;
 }
@@ -431,6 +433,26 @@ BYTE *VSurfaceLockOld(struct VSurface *vs, u32 *pitch) {
   struct BufferLockInfo res = VSurfaceLock(vs);
   *pitch = res.pitch;
   return res.dest;
+}
+
+BOOLEAN SetVideoSurfaceTransparency(UINT32 uiIndex, COLORVAL TransColor) {
+  struct VSurface *hVSurface;
+
+  //
+  // Get Video Surface
+  //
+
+  if (!(GetVideoSurface(&hVSurface, uiIndex))) {
+    return FALSE;
+  }
+
+  //
+  // Set transparency
+  //
+
+  SetVideoSurfaceTransparencyColor(hVSurface, TransColor);
+
+  return (TRUE);
 }
 
 struct VSurface *GetVSByID(VSurfID id) {

ja2lib/SGP/VSurface.h
@@ -77,6 +77,9 @@ struct VSurface *GetVSByID(VSurfID id);
 
 BOOLEAN VSurfaceColorFill(struct VSurface *dest, i32 x1, i32 y1, i32 x2, i32 y2, u16 Color16BPP);
 
+// Sets transparency
+BOOLEAN SetVideoSurfaceTransparency(UINT32 uiIndex, COLORVAL TransColor);
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 //
 // Video Surface manipulation functions
@@ -86,6 +89,9 @@ BOOLEAN VSurfaceColorFill(struct VSurface *dest, i32 x1, i32 y1, i32 x2, i32 y2,
 // Set data from struct Image*.
 BOOLEAN SetVideoSurfaceDataFromHImage(struct VSurface *hVSurface, struct Image *hImage, UINT16 usX,
                                       UINT16 usY, struct GRect *pSrcRect);
+
+// Sets Transparency color into HVSurface and the underlying DD surface
+BOOLEAN SetVideoSurfaceTransparencyColor(struct VSurface *hVSurface, COLORVAL TransColor);
 
 // Deletes all data, including palettes, regions, DD Surfaces
 BOOLEAN DeleteVideoSurface(struct VSurface *hVSurface);

ja2lib/SGP/VSurfaceInternal.h
@@ -8,8 +8,9 @@ struct VSurface {
   UINT16 usHeight;
   UINT16 usWidth;
 
-  void* _platformData1;  // platform-specific data
-  void* _platformData2;  // platform-specific data
+  void* _platformData1;       // platform-specific data
+  void* _platformData2;       // platform-specific data
+  COLORVAL TransparentColor;  // Defaults to 0,0,0
 };
 
 #endif

ja2lib/Tactical/Interface.c
@@ -1479,6 +1479,16 @@ void RenderOverlayMessage(VIDEO_OVERLAY *pBlitter) {
                    pBlitter->sY + gusOverlayPopupBoxHeight);
 }
 
+void EndOverlayMessage() {
+  if (giPopupSlideMessageOverlay != -1) {
+    //		DebugMsg( TOPIC_JA2, DBG_ERROR, String( "Removing Overlay message") );
+
+    RemoveVideoOverlay(giPopupSlideMessageOverlay);
+
+    giPopupSlideMessageOverlay = -1;
+  }
+}
+
 void DrawBarsInUIBox(struct SOLDIERTYPE *pSoldier, INT16 sXPos, INT16 sYPos, INT16 sWidth,
                      INT16 sHeight) {
   FLOAT dWidth, dPercentage;
@@ -2155,6 +2165,9 @@ void InternalBeginUIMessage(BOOLEAN fUseSkullIcon, CHAR16 *pFontString, ...) {
 
   // Override it!
   OverrideMercPopupBox(&gpUIMessageOverrideMercBox);
+
+  // SetPrepareMercPopupFlags( MERC_POPUP_PREPARE_FLAGS_TRANS_BACK |
+  // MERC_POPUP_PREPARE_FLAGS_MARGINS );
 
   if (fUseSkullIcon) {
     SetPrepareMercPopupFlags(MERC_POPUP_PREPARE_FLAGS_MARGINS | MERC_POPUP_PREPARE_FLAGS_SKULLICON);

ja2lib/Tactical/Interface.h
@@ -126,6 +126,8 @@ void CancelOpenDoorMenu();
 
 void HandleInterfaceBackgrounds();
 
+void EndOverlayMessage();
+
 void DrawSelectedUIAboveGuy(UINT16 usSoldierID);
 
 void CreateCurrentTacticalPanelButtons();

ja2lib/Utils/MercTextBox.c
@@ -369,6 +369,10 @@ INT32 PrepareMercPopupBox(INT32 iBoxId, UINT8 ubBackgroundIndex, UINT8 ubBorderI
   DestRect.iBottom = DestRect.iTop + usHeight;
 
   if (pPopUpTextBox->uiFlags & MERC_POPUP_PREPARE_FLAGS_TRANS_BACK) {
+    // Zero with yellow,
+    // Set source transparcenty
+    SetVideoSurfaceTransparency(pPopUpTextBox->uiSourceBufferIndex, FROMRGB(255, 255, 0));
+
     pDestBuf =
         (UINT16 *)VSurfaceLockOld(GetVSByID(pPopUpTextBox->uiSourceBufferIndex), &uiDestPitchBYTES);
 

platform-dummy/dummy-platform.c
@@ -148,6 +148,10 @@ BOOLEAN Set8BPPPalette(struct SGPPaletteEntry *pPalette) { return FALSE; }
 
 void FatalError(STR8 pError, ...) {}
 
+BOOLEAN SetVideoSurfaceTransparencyColor(struct VSurface *hVSurface, COLORVAL TransColor) {
+  return FALSE;
+}
+
 struct VSurface *CreateVideoSurface(u16 width, u16 height) { return NULL; }
 
 BOOLEAN DeleteVideoSurface(struct VSurface *hVSurface) { return FALSE; }

platform-linux/platform-linux-stubs.c
@@ -150,6 +150,10 @@ BOOLEAN Set8BPPPalette(struct SGPPaletteEntry *pPalette) { return FALSE; }
 
 void FatalError(STR8 pError, ...) {}
 
+BOOLEAN SetVideoSurfaceTransparencyColor(struct VSurface *hVSurface, COLORVAL TransColor) {
+  return FALSE;
+}
+
 struct VSurface *CreateVideoSurface(u16 width, u16 height) { return NULL; }
 
 BOOLEAN DeleteVideoSurface(struct VSurface *hVSurface) { return FALSE; }

platform-win32/Video_win.c
@@ -1501,6 +1501,7 @@ struct VSurface *CreateVideoSurface(u16 width, u16 height) {
 
   vs->usHeight = height;
   vs->usWidth = width;
+  vs->TransparentColor = FROMRGB(0, 0, 0);
   vs->_platformData1 = (PTR)lpDDS;
   vs->_platformData2 = (PTR)lpDDS2;
 
@@ -1519,6 +1520,32 @@ void VSurfaceUnlock(struct VSurface *vs) {
   if (vs) {
     IDirectDrawSurface2_Unlock((LPDIRECTDRAWSURFACE2)vs->_platformData2, NULL);
   }
+}
+
+// Transparency needs to take RGB value and find best fit and place it into DD Surface
+// colorkey value.
+BOOLEAN SetVideoSurfaceTransparencyColor(struct VSurface *hVSurface, COLORVAL TransColor) {
+  DDCOLORKEY ColorKey;
+  LPDIRECTDRAWSURFACE2 lpDDSurface;
+
+  // Assertions
+  Assert(hVSurface != NULL);
+
+  // Set trans color into Video Surface
+  hVSurface->TransparentColor = TransColor;
+
+  // Get surface pointer
+  lpDDSurface = (LPDIRECTDRAWSURFACE2)hVSurface->_platformData2;
+  if (!(lpDDSurface != NULL)) {
+    return FALSE;
+  }
+
+  ColorKey.dwColorSpaceLowValue = Get16BPPColor(TransColor);
+  ColorKey.dwColorSpaceHighValue = ColorKey.dwColorSpaceLowValue;
+
+  DDSetSurfaceColorKey(lpDDSurface, DDCKEY_SRCBLT, &ColorKey);
+
+  return (TRUE);
 }
 
 // Deletes all palettes, surfaces and region data